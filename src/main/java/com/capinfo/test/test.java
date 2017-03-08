86ab274677ed5d336748168bd0907c0702b4516e
FeierV587 <1179858510@qq.com>
2017-03-08 16:38:27 +0800

    First test

---
+++ src/main/java/com/capinfo/test/test.java
@@ -0,0 +1,17 @@
+package com.capinfo.test;
+
+/**
+ * Created by Administrator on 2017/3/8.
+ */
+
+
+
+
+public class test {
+    public  void  test(){
+        System.out.print("aaaaaaa");
+    }
+
+
+}
+
